1f5817ab8d32d708fe9a82db8d691ef2ac966b94
rakeshmakwana84 <86312285+rakeshmakwana84@users.noreply.github.com>
2025-08-12 20:50:18 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
 
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Fitness App Works!</h1>
+    </div>
+  )
+}
